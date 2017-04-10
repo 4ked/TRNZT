3143f5c5d07aec34c40f17f7a2f11cda76e18850
4ked <max.goeke0@gmail.com>
2017-04-10 11:07:54 -0500

    .gitignore addition

--- Server/server.js
+++ Server/server.js
@@ -8,19 +8,16 @@ var app = express();
 var server = http.Server(app);
 var io = socketio(server);
 
-// pass web server to client
-app.use('/', express.static('../Client/'));
+const path = require('path');
 
-<<<<<<< HEAD
-server.listen(3000, function(){
-  	console.log('listening on *:3000');
-});
-=======
 app.use('/', express.static('../Client/'));
->>>>>>> d4fa947264cb89ee8b0896337dd135c16471fba7
 
 io.on('connection', function(socket){
-  	socket.on('chat message', function(msg){
-    	io.emit('chat message', msg);
+    socket.on('chat message', function(msg){
+    	console.log('message: ' + msg);
   	});
+});
+
+server.listen(3000, function(){
+  	console.log('listening on *:3000');
 });
