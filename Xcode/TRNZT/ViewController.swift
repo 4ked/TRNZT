0bdeb0c7f7a7364367d894ee5f6b1b01eb3ac04f
4ked <max.goeke0@gmail.com>
2017-04-15 12:20:24 -0500

    uber API start

--- Xcode/TRNZT/ViewController.swift
+++ Xcode/TRNZT/ViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let page = Bundle.main.path(forResource: "WebClient/Client/index", ofType: "html")!
+        var content: String?
+        do {
+            content = try String(contentsOfFile: page, encoding: String.Encoding.utf8)
+        } catch let error as NSError {
+            print("error: \(error)")
+        }
+        
+        let base = Bundle.main.path(forResource: "WebClient/Client/css/main", ofType: "css")!
+        let baseUrl = URL(fileURLWithPath: base)
+        webView.loadHTMLString(content!, baseURL: baseUrl)
     }
 
 
 }
+
 
